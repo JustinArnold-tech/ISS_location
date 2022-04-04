dc55e608ca3833b6636284346577bd0493760168
JustinArnold-tech <79416942+JustinArnold-tech@users.noreply.github.com>
2022-04-04 14:33:38 -0400

    code for app and html

---
+++ app.py
@@ -0,0 +1,30 @@
+import requests
+from flask import Flask, render_template
+
+server = Flask(__name__)
+PRIVATE_TOKEN = "pk.addf779a6ccc6dc99c20e9c74f39da2f"
+url = "http://api.open-notify.org/iss-now.json"
+
+@server.route("/", methods=['GET'])
+def iss_local():
+
+    req = requests.get(url)
+
+    obj = req.json()
+    lat = obj['iss_position']['latitude']
+    lon = obj['iss_position']['longitude']
+    # print(lat, lon)
+    try:
+        iss_local = requests.get(f'https://us1.locationiq.com/v1/reverse.php?key={PRIVATE_TOKEN}&lat={lat}&lon={lon}&format=json')
+    except Exception as e:
+        raise e
+    
+    iss_curr = iss_local.json()
+    # print(iss_curr)
+    state = iss_curr['address']['state']
+    country = iss_curr['address']['country']
+
+    return render_template("iss.html",country=country, state=state)
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', debug=True)
