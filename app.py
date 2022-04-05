a86161e0bdfa7012f881717182da11cc8e3330bf
JustinArnold-tech <79416942+JustinArnold-tech@users.noreply.github.com>
2022-04-05 10:47:51 -0400

    KEY

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+import os
 import requests
 from flask import Flask, render_template
 
 server = Flask(__name__)
-PRIVATE_TOKEN = "pk.addf779a6ccc6dc99c20e9c74f39da2f"
+PRIVATE_TOKEN = os.getenv('PRIVATE_TOKEN')
 url = "http://api.open-notify.org/iss-now.json"
 
 @server.route("/", methods=['GET'])
