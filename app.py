223fa4f0d7e65b1deead92a3014dc52f8bb1a1f8
JustinArnold-tech <79416942+JustinArnold-tech@users.noreply.github.com>
2022-04-05 11:37:20 -0400

    put in notes

--- app.py
+++ app.py
@@ -11,19 +11,21 @@ url2 = "http://api.open-notify.org/astros.json"
 @server.route("/", methods=['GET'])
 def iss_local():
 
+    #Gets the latitude and longitude of the ISS
     req = requests.get(url)
 
     obj = req.json()
     lat = obj['iss_position']['latitude']
     lon = obj['iss_position']['longitude']
-    # print(lat, lon)
+    
+    #Takes the latitude and longitude and puts it into location api to find state and country the ISS is over
     try:
         iss_local = requests.get(f'https://us1.locationiq.com/v1/reverse.php?key={PRIVATE_TOKEN}&lat={lat}&lon={lon}&format=json')
     except Exception as e:
         raise e
     
     iss_curr = iss_local.json()
-    # print(iss_curr)
+    
     if 'address' in iss_curr:
         state = iss_curr['address']['state']
         country = iss_curr['address']['country']
@@ -31,14 +33,15 @@ def iss_local():
         state = "Over"
         country = "Water"
 
+    #Gets the number of people currently in space
     req2 = requests.get(url2)
     
     obj2 = req2.json()
     cnt = obj2['number']
 
+    #Renders to info to html
     return render_template("iss.html",country=country, state=state, lat=lat, lon=lon, cnt=cnt)
     
-        
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0', debug=True)
