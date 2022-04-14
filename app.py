5667c5832d03b061dbdddf1c9e404dad947a26be
JustinArnold-tech <79416942+JustinArnold-tech@users.noreply.github.com>
2022-04-14 14:18:59 -0400

    changing app to movie app

--- app.py
+++ app.py
@@ -1,44 +1,60 @@
 import os
 import requests
-from flask import Flask, render_template
+from pprint import pprint
+from flask import Flask, render_template, request
+
 
 server = Flask(__name__)
-LOCATION_API_TOKEN = os.getenv('LOCATION_API_TOKEN')
-url = "http://api.open-notify.org/iss-now.json"
-url2 = "http://api.open-notify.org/astros.json"
+apiKey = os.getenv('apiKey')
+POSTER_KEY = os.getenv("k_36k6exf5")
 
-@server.route("/", methods=['GET'])
-def iss_local():
-
-    #Gets the latitude and longitude of the ISS
-    req = requests.get(url)
-
-    obj = req.json()
-    lat = obj['iss_position']['latitude']
-    lon = obj['iss_position']['longitude']
+@server.route("/", methods=['Get', 'Post'])
+def api_weather():
     
-    #Takes the latitude and longitude and puts it into location api to find which country the ISS is over
-    try:
-        iss_local = requests.get(f'https://us1.locationiq.com/v1/reverse.php?key={LOCATION_API_TOKEN}&lat={lat}&lon={lon}&format=json')
-    except Exception as e:
-        raise e
-    
-    iss_curr = iss_local.json()
-    print(iss_curr)
-    if 'address' in iss_curr:
-        country = iss_curr['address']['country']
-    else:
-        country = "Over Water"
+    if request.method == 'POST':
+        
+        data_URL = 'http://www.omdbapi.com/?apikey='+apiKey
+        year = ''
+        movie = request.form['movie'] 
+        params = {
+            't':movie,
+            'type':'movie',
+            'y':year,
+            'plot':'full'
+        }
+        response = requests.get(data_URL,params=params).json()
 
-    #Gets the number of people currently in space
-    req2 = requests.get(url2)
-    
-    obj2 = req2.json()
-    cnt = obj2['number']
+        Title = response['Title']
+        released = response['Released']
+        Rating = response['Rated']
+        Runtime = response['Runtime']
+        Genre = response['Genre']
+        Director = response['Director']
+        Writer = response['Writer']
+        Actors = response['Actors']
+        Plot = response['Plot']
+        Id = response['imdbID']
+        
+        info = { 
+            'Title' : Title,
+            'released' : released,
+            'Rating' : Rating,
+            'Runtime' : Runtime, 
+            'Genre' : Genre,
+            'Director' : Director,
+            'Writer' : Writer,
+            'Actors' : Actors,
+            'Plot' : Plot,
+        }
+        try:
+            poster_info = requests.get(f'https://imdb-api.com/en/API/Posters/{POSTER_KEY}/{Id}').json()
+        except Exception as e:
+            raise e
+        pprint(poster_info)
+        poster = poster_info['posters'][0]['link']
+        return render_template('movie.html', poster=poster, **info)
 
-    #Renders to info to html
-    return render_template("iss.html",country=country, lat=lat, lon=lon, cnt=cnt)
-    
+    return render_template('index.html')
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0', debug=True)
