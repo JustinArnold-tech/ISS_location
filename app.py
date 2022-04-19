191e137a362977549a89b7a2bf2f7467b370c4d2
JustinArnold-tech <79416942+JustinArnold-tech@users.noreply.github.com>
2022-04-19 13:00:00 -0400

    adding a log

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import logging as logg
 from pprint import pprint
 from flask import Flask, render_template, request
 
@@ -7,6 +8,9 @@ from flask import Flask, render_template, request
 server = Flask(__name__)
 apiKey = os.getenv('apiKey')
 POSTER_KEY = os.getenv("POSTER_KEY")
+LOG_FILE = 'events.log'
+
+logg.basicConfig(filename=LOG_FILE, encoding='utf-8', level=logg.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 @server.route("/", methods=['Get', 'Post'])
 def api_weather():
